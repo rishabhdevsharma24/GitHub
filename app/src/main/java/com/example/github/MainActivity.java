b371fa6d818e59090e071bc4e7f8dddc76064dc3
rishabhdevsharma24 <rishabhdevsharma9@gmail.com>
2021-03-11 13:26:56 +0530

    Revert "This is Our Third Commit"
    This reverts commit 12527c73

--- app/src/main/java/com/example/github/MainActivity.java
+++ app/src/main/java/com/example/github/MainActivity.java
@@ -10,6 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        fgghfdcghggvvn
+
+        System.out.println("HAr HAr Mahadev");
+
+
     }
 }
